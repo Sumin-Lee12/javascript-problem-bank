37cb075bfc150fc8e958a77d0279a4fdf4af6418
Sumin-Lee12 <leesumin041@gmail.com>
2025-02-13 09:58:01 +0900

    2/13/2025 문제은행 풀이

--- part2/02.배열/(lv.2)문자열-뒤집기.js
+++ part2/02.배열/(lv.2)문자열-뒤집기.js
@@ -9,7 +9,18 @@
  * @returns {string}
  */
 
-function reverseString(str) {}
+function reverseString(str) {
+  newStr = "";
+
+  if (typeof(str) === "string") {
+    for (let i = str.length-1; i >= 0; i--) { // str.length-1인 이유가, str의 length는 5인 반면에, index는 0으로 시작해 4로 끝난다. 그래서 index에 맞추기 위해 str.length-1이라 적은 것!
+      newStr += str[i]; // 위에서 i >= 0 인 이유: i = 0은 할당 연사자이기 때문에 i = 0 -> 0 = false가 되어버려 반복문 실행 자체가 안 된다.
+    }
+  } else {
+    return "";
+  }
+  return newStr;
+}
 
 // export 를 수정하지 마세요.
 export { reverseString };
